c1701b9fc24b1716873d0392f4c906ab77d29661
zeeshanm0x0 <129337199+zeeshanm0x0@users.noreply.github.com>
2023-03-30 03:08:11 +0500

    Update x-officers.py

--- x-officers.py
+++ x-officers.py
@@ -3,11 +3,12 @@
 import argparse
 import requests
 import re
+import sys
 
 parser = argparse.ArgumentParser(description="Find JavaScript files containing a specific keyword from a list of URLs")
-parser.add_argument("-f", "--file", help="File containing URLs to check", required=True)
 parser.add_argument("-k", "--keyword", help="Keyword to search for in JavaScript files", required=True)
 parser.add_argument("-t", "--timeout", help="Request timeout in seconds (default: 5)", type=int, default=5)
+parser.add_argument("url", help="URL or file containing URLs to check", nargs="?", default=sys.stdin)
 args = parser.parse_args()
 
 def get_javascript_urls(url):
@@ -33,8 +34,16 @@ def search_for_keyword(url, keyword):
 
     return keyword in response.text
 
-with open(args.file, "r") as f:
-    urls = [line.strip() for line in f]
+urls = []
+
+if isinstance(args.url, str):
+    # Read URLs from file
+    with open(args.url, "r") as f:
+        urls = [line.strip() for line in f]
+else:
+    # Read URLs from standard input
+    for line in args.url:
+        urls.append(line.strip())
 
 javascript_urls = []
 
@@ -46,4 +55,3 @@ for url in urls:
 
 if len(javascript_urls) == 0:
     print("No JavaScript files containing \"" + args.keyword + "\" were found.")
-
